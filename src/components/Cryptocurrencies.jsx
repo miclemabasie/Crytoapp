1f057b7d70f147b4864e672e52845477acf6dec8
miclemabasie <51621036+miclemabasie@users.noreply.github.com>
2022-04-03 06:18:46 +0100

    Update Cryptocurrencies.jsx

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -30,7 +30,7 @@ const Cryptocurrencies = ({simplified}) => {
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency, i) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}>
-            <Link to={`crypto/${currency.uuid}`}>
+            <Link to={`/crypto/${currency.uuid}`}>
               <Card title={`${currency.rank}. ${currency.name}`}
                     extra={<img className="crypto-image" src={currency.iconUrl} />}
                     hoverable
